c54cf49a495d008057ee07ab79ad32092b40f5d6
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2020-12-05 08:46:11 +0900

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Yuki Tsukada on 2020/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
