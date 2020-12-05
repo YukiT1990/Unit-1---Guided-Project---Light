ac4a048f6c41578081acd3c44c3cbe2dd6da61ea
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2020-12-05 10:04:30 +0900

    Unit 1 - Guided Project - Light

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var lightOn = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+//        if lightOn {
+//            view.backgroundColor = .white
+//        } else {
+//            view.backgroundColor = .black
+//        }
+        // ternary operator
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 }
 
